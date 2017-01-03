d8f92d01f232de2250ef75809a2392855dfd9d51
iamshaunjp <shaunjp@shaun-pelling.com>
2017-01-03 11:18:57 +0000

    lesson 5 code added

---
+++ test/demo_test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// Describe our tests
+describe('testing mocha', function(){
+
+  // Create tests
+  it('adds numbers that equal to 5', function(){
+    assert(2 + 3 === 5);
+  });
+
+});
