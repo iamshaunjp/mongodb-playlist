4ade209debfe59c986e378c54399d7d07d4c70f2
iamshaunjp <shaunjp@shaun-pelling.com>
2017-01-03 16:12:56 +0000

    added lesson 6 code

---
+++ test/saving_test.js
@@ -0,0 +1,22 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+// Describe our tests
+describe('Saving records', function(){
+
+  // Create tests
+  it('Saves a record to the database', function(done){
+
+    const char = new MarioChar({
+      name: 'Mario'
+    });
+
+    char.save().then(function(){
+      assert(!char.isNew);
+      done();
+    });
+
+  });
+
+});
