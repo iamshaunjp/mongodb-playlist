35f95d7d0346417a29475fe2afedeeb67fbfc1d8
iamshaunjp <shaunjp@shaun-pelling.com>
2017-01-04 13:15:33 +0000

    added lesson 11 code

--- test/finding_test.js
+++ test/finding_test.js
@@ -3,15 +3,15 @@ const MarioChar = require('../models/mariochar');
 
 // Describe our tests
 describe('Finding records', function(){
-
+  var char;
   // Add a character to the db before each tests
   beforeEach(function(done){
-    const char = new MarioChar({
+    char = new MarioChar({
       name: 'Mario'
     });
     char.save().then(function(){
       done();
-    })
+    });
   });
 
   // Create tests
@@ -20,7 +20,13 @@ describe('Finding records', function(){
       assert(result.name === 'Mario');
       done();
     });
+  });
 
+  it('Finds a record by unique id', function(done){
+    MarioChar.findOne({_id: char._id}).then(function(result){
+      assert(result._id.toString() === char._id.toString());
+      done();
+    });
   });
 
 });
