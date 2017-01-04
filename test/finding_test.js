cd93aa9a9bb82f18b707fe784216add2ba9e67e6
iamshaunjp <shaunjp@shaun-pelling.com>
2017-01-04 12:45:41 +0000

    added lesson 10 code

---
+++ test/finding_test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+// Describe our tests
+describe('Finding records', function(){
+
+  // Add a character to the db before each tests
+  beforeEach(function(done){
+    const char = new MarioChar({
+      name: 'Mario'
+    });
+    char.save().then(function(){
+      done();
+    })
+  });
+
+  // Create tests
+  it('Finds a record from the database', function(done){
+    MarioChar.findOne({name: 'Mario'}).then(function(result){
+      assert(result.name === 'Mario');
+      done()
+    });
+
+  });
+
+});
