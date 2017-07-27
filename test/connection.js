3efa3efa93959cc68bb28097f83e770b045d6989
Edwin Moses <workockmoses@gmail.com>
2017-07-27 16:39:34 +0300

    mocha tests models

---
+++ test/connection.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/testaroo');
+
+mongoose.connection.once('open', () => {
+  console.log("Connection to database was succesfull!");
+}).on('error', (error) => {
+  console.log("Connection error:", error);
+});
