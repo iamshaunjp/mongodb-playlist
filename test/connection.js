a1e2d9cef8f29f745af5fc1706f7660a323114e4
Edwin Moses <workockmoses@gmail.com>
2017-07-27 19:54:08 +0300

    Revert to es6

--- test/connection.js
+++ test/connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/testaroo');
 
-mongoose.connection.once('open', () => {
-  console.log("Connection to database was succesfull!");
-}).on('error', (error) => {
-  console.log("Connection error:", error);
+mongoose.connection.once('open', function () {
+  console.log('Connection to database was succesfull!');
+}).on('error', function (error) {
+  console.log('Connection error:', error);
 });
