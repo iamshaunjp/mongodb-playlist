6cdb2fdc102a9f2d6c86bdf2ab4bdba6b40f0a18
Edwin Moses <workockmoses@gmail.com>
2017-07-28 13:14:53 +0300

    update test run

--- test/updating_test.js
+++ test/updating_test.js
@@ -1,32 +1,31 @@
-var assert = require('assert');
-var MarioChar = require('../models/mariochar');
+var assert = require("assert");
+var MarioChar = require("../models/mariochar");
 
 //Create test
-describe('Delete records in the database', function () {
+describe("Updating records in the database", function() {
   //Global var
   var char;
   //Create record to database to delete
-  beforeEach(function (done) {
+  beforeEach(function(done) {
     char = new MarioChar({
       name: "Mario"
     });
 
-    char.save().then(function () {
-      assert(char.isNew === false);
+    char.save().then(function() {
       done();
     });
-
   });
 
-  
   //Delete test
-  it('Delete one record in the database', function (done) {
-    MarioChar.findOneAndRemove({ name: "Mario" }).then(function () {
-      MarioChar.findOne({ name: "Mario" }).then(function (result) {
-        assert(result == null);
+  it("Update one record in the database", function(done) {
+    MarioChar.findOneAndUpdate(
+      { name: "Mario" },
+      { name: "Mike" }
+    ).then(function() {
+      MarioChar.findOne({ _id: char._id }).then(function(result) {
+        assert(result.name === "Mike");
         done();
       });
     });
   });
-
 });
