8f5f083380c8dc95ddd3bf532c6b921fcbf5f37f
Edwin Moses <workockmoses@gmail.com>
2017-07-28 10:39:47 +0300

    removed trailing whitespace

--- models/mariochar.js
+++ models/mariochar.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
 var MarioCharSchema = new Schema({
   name: String,
-  weight: Number,
+  weight: Number
 });
 
-var MarioChar = mongoose.model('mariochar', MarioCharSchema);
+var MarioChar = mongoose.model("mariochar", MarioCharSchema);
 
 module.exports = MarioChar;
