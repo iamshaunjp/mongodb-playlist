b4d4d9b6708aa447b730ff4c160d04b2c3a107b6
Edwin Moses <workockmoses@gmail.com>
2017-07-28 12:21:54 +0300

    find by id property test

--- test/find_test.js
+++ test/find_test.js
@@ -3,9 +3,11 @@ var MarioChar = require('../models/mariochar');
 
 //Create test
 describe('Find records in the database', function () {
+  //Global var
+  var char;
   //Create record to database to find
   beforeEach(function (done) {
-    var char = new MarioChar({
+    char = new MarioChar({
       name: "Mario"
     });
 
@@ -18,10 +20,18 @@ describe('Find records in the database', function () {
 
   
   //Find test
-  it('Find one record in the database', function () {
+  it('Find one record in the database', function (done) {
     MarioChar.findOne({ name: "Mario" }).then(function (result) {
       assert(result.name === "Mario");
       done();
     });
   });
+
+  //Find one record in the databse by ID
+  it('Find record by its ID', function (done) {
+    MarioChar.findOne({ _id: char._id }).then(function (result) {
+      assert(result._id.toString() === char._id.toString());
+      done();
+    });
+  })
 });
