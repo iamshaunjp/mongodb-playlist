e82a2f6539603621596bc5050aec27d24b3a6929
Edwin Moses <workockmoses@gmail.com>
2017-07-28 12:08:55 +0300

    find record test

--- test/find_test.js
+++ test/find_test.js
@@ -8,15 +8,17 @@ describe('Find records in the database', function () {
     var char = new MarioChar({
       name: "Mario"
     });
+
+    char.save().then(function () {
+      assert(char.isNew === false);
+      done();
+    });
+
   });
 
-  char.save().then(function () {
-    assert(char.isNew === false);
-    done();
-  });
-
+  
   //Find test
-  it('Find ine record in the database', function () {
+  it('Find one record in the database', function () {
     MarioChar.findOne({ name: "Mario" }).then(function (result) {
       assert(result.name === "Mario");
       done();
