3c6bb01a67b28dee46d4864512ea9f5115e4a29c
Edwin Moses <workockmoses@gmail.com>
2017-07-28 12:52:38 +0300

    update test run

---
+++ test/updating_test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var MarioChar = require('../models/mariochar');
+
+//Create test
+describe('Delete records in the database', function () {
+  //Global var
+  var char;
+  //Create record to database to delete
+  beforeEach(function (done) {
+    char = new MarioChar({
+      name: "Mario"
+    });
+
+    char.save().then(function () {
+      assert(char.isNew === false);
+      done();
+    });
+
+  });
+
+  
+  //Delete test
+  it('Delete one record in the database', function (done) {
+    MarioChar.findOneAndRemove({ name: "Mario" }).then(function () {
+      MarioChar.findOne({ name: "Mario" }).then(function (result) {
+        assert(result == null);
+        done();
+      });
+    });
+  });
+
+});
