697206c64d8e61aa1c08dd97949452ecbf6f753d
Edwin Moses <workockmoses@gmail.com>
2017-07-28 10:12:21 +0300

    mocha tests

---
+++ test/demo_test.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha');
+var assert = require('assert');
+
+//Describe test
+describe('Some demo test', function () {
+
+  //Create test
+  it('Adds two numbers', function () {
+    assert(3 + 4 === 7);
+  });
+});
